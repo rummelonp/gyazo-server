1d8324f69759b64ee925e6ec5496c258d7c981f2
Kazuya Takeshima <mail@mitukiii.jp>
2012-12-16 00:30:01 +0900

    rails new .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pic::Application.config.secret_token = '486cda695466dc06343fbde82e741f45fdac817959c2261291192090d69dccdd7bfa808cbbe61cc8067059d8fa2392b09b180c99e91d6b07c13a76f624b13bcf'
