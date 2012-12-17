00126b15074e0785be54c4ceebcaf496ef66d302
Kazuya Takeshima <mail@mitukiii.jp>
2012-12-17 19:38:00 +0900

    Make thumnail when upload.

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -8,6 +8,11 @@ class UploadController < ApplicationController
       raise ImageableError.new('Image Data Not Found.')
     end
     image = Image.create(data.read, data.original_filename)
+    begin
+      image.make_thumbnail
+    rescue => e
+      logger.info e
+    end
     url = "#{request.protocol}#{request.host_with_port}#{image.path}"
     render :text => url
   end
