2adf852aaa866147ad7f789854e68e8e973618d1
Kazuya Takeshima <mail@mitukiii.jp>
2012-12-17 13:39:12 +0900

    Add log to errar handle.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,10 @@ class ApplicationController < ActionController::Base
     @message = e.message
     case e
     when ImageableNotFound
+      logger.info e
       render :template => 'error/not_found.html.erb', :status => 404
     else
+      logger.error e
       render :template => 'error/client.html.erb', :status => 400
     end
   end
