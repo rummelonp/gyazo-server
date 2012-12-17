6268acbaafb49543023caab3306015217df7504e
Kazuya Takeshima <mail@mitukiii.jp>
2012-12-17 13:43:41 +0900

    Delete secret token.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Pic::Application.config.secret_token = '10cb362836f29f089f60cc24ef4eb1052cd644c7e18e500a0928f94dbb7cb0d031572c0bbae2d360b6ed176632921e54d23794a9ca3d68e3135d84a6f6ca302d'
+Pic::Application.config.secret_token = '*** secret token ***'
